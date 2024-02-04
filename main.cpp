0865485e889116fb72aeb817dc673f0f38206746
159Dima <131018592+159Dima@users.noreply.github.com>
2024-02-04 13:56:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,207 @@
-#include <algorithm>
 #include <cassert>
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <string>
 #include <vector>
+#include <algorithm>
 
+// Умный указатель, удаляющий связанный объект при своём разрушении.
+// Параметр шаблона T задаёт тип объекта, на который ссылается указатель
 using namespace std;
-using filesystem::path;
+template <typename T>
+class PtrVector {
+public:
+    PtrVector() = default;
 
-path operator""_p(const char* data, std::size_t sz) {
-    return path(data, data + sz);
-}
-struct file_pahh_status {
-    path file;
-    filesystem::file_status status;
-};
-
-// напишите эту функцию
-void PrintTree(ostream& dst, const path& p, int offset) {
-
-    vector<file_pahh_status>name_file;
-    for (const auto& dir_entry : filesystem::directory_iterator(p)) {
-        name_file.push_back({ dir_entry.path(), dir_entry.status() });
-    }
-    reverse(name_file.begin(), name_file.end());
-    for (const auto& dir : name_file) {
-        dst << string(offset, ' ') << dir.file.filename().string() << endl;
-        //cout << string(offset, ' ') << dir.file.filename().string() << endl;
-        if (dir.status.type() == filesystem::file_type::directory) {
-            PrintTree(dst, dir.file, offset + 2);
+    // Создаёт вектор указателей на копии объектов из other
+    PtrVector(const PtrVector& other) {
+        // Реализуйте копирующий конструктор самостоятельно
+        items_.reserve(other.GetItems().size());
+        for (auto object : other.GetItems()) {
+            
+            items_.push_back((object == nullptr) ? nullptr : (new T (*object)));
         }
     }
-}
 
-void PrintTree(ostream& dst, const path& p)
-{
-    int count_ = 2;
-    dst << p.filename().string() << endl;
-    //cout << p.filename().string() << endl;
-    PrintTree(dst, p, count_);
-}
+    // Деструктор удаляет объекты в куче, на которые ссылаются указатели,
+    // в векторе items_
+    ~PtrVector() {
+        // Реализуйте тело деструктора самостоятельно
+        for (auto& znach : items_) {
+            delete znach;
+        }
 
+    }
+
+    // Возвращает ссылку на вектор указателей
+    std::vector<T*>& GetItems() noexcept {
+        // Реализуйте метод самостоятельно
+        return items_;
+    }
+
+    // Возвращает константную ссылку на вектор указателей
+    std::vector<T*> const& GetItems() const noexcept {
+        // Реализуйте метод самостоятельно
+        return items_;
+    }
+    PtrVector& operator=(const PtrVector& rhs) {
+        if (this != &rhs) {
+            // Реализация операции присваивания с помощью идиомы Copy-and-swap.
+            // Если исключение будет выброшено, то на текущий объект оно не повлияет.
+            auto rhs_copy(rhs);
+
+            // rhs_copy содержит копию правого аргумента.
+            // Обмениваемся с ним данными.
+            swap(rhs_copy);
+
+            // Теперь текущий объект содержит копию правого аргумента,
+            // а rhs_copy - прежнее состояние текущего объекта, которое при выходе
+            // из блока будет разрушено.
+        }
+
+        return *this;
+    }
+private:
+    void swap(PtrVector& other) noexcept {
+        items_.swap(other.GetItems());
+    }
+
+
+
+    std::vector<T*> items_;
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+class Tentacle {
+public:
+    explicit Tentacle(int id) noexcept
+        : id_(id) {
+    }
+
+    int GetId() const noexcept {
+        return id_;
+    }
+
+    Tentacle* GetLinkedTentacle() const noexcept {
+        return linked_tentacle_;
+    }
+    void LinkTo(Tentacle& tentacle) noexcept {
+        linked_tentacle_ = &tentacle;
+    }
+    void Unlink() noexcept {
+        linked_tentacle_ = nullptr;
+    }
+
+private:
+    int id_ = 0;
+    Tentacle* linked_tentacle_ = nullptr;
+};
+
+// Осьминог
+class Octopus {
+public:
+    Octopus()
+        : Octopus(8) {
+    }
+
+    explicit Octopus(int num_tentacles) {
+        Tentacle* t = nullptr;
+        for (int i = 1; i <= num_tentacles; ++i) {
+            t = new Tentacle(i);
+            tentacles_.GetItems().push_back(t);
+            t = nullptr;
+        }
+    }
+
+    ~Octopus() {
+        Cleanup();
+    }
+    Octopus& operator=(const Octopus& rhs) {
+        if (this != &rhs) {
+            tentacles_ = rhs.getV();
+        }
+
+        return *this;
+    }
+    Tentacle& AddTentacle() {
+        //Реализуйте добавление щупальца самостоятельно
+        Tentacle* t = new Tentacle(tentacles_.GetItems().size() + 1);
+        tentacles_.GetItems().push_back(t);
+        return *t;
+    }
+
+    int GetTentacleCount() const noexcept {
+        return static_cast<int>(tentacles_.GetItems().size());
+    }
+    PtrVector<Tentacle> getV() const {
+        return  tentacles_;
+    }
+    const Tentacle& GetTentacle(size_t index) const {
+        return *tentacles_.GetItems().at(index);
+    }
+    Tentacle& GetTentacle(size_t index) {
+        return *tentacles_.GetItems().at(index);
+    }
+
+private:
+    void Cleanup() {
+        // Удаляем щупальца осьминога из динамической памяти
+        for (Tentacle* t : tentacles_.GetItems()) {
+            delete t;
+        }
+        // Очищаем массив указателей на щупальца
+        tentacles_.GetItems().clear();
+    }
+
+
+    // Вектор хранит указатели на щупальца. Сами объекты щупалец находятся в куче
+    PtrVector<Tentacle> tentacles_;
+};
+// Эта функция main тестирует шаблон класса PtrVector
 int main() {
-    error_code err;
-    filesystem::remove_all("test_dir", err);
-    filesystem::create_directories("test_dir"_p / "a"_p, err);
-    filesystem::create_directories("test_dir"_p / "b"_p, err);
+    // Проверка присваивания осьминогов
+    {
+        Octopus octopus1(3);
 
-    ofstream("test_dir"_p / "b"_p / "f1.txt"_p);
-    ofstream("test_dir"_p / "b"_p / "f2.txt"_p);
-    ofstream("test_dir"_p / "a"_p / "f3.txt"_p);
+        // Настраиваем состояние исходного осьминога
+        octopus1.GetTentacle(2).LinkTo(octopus1.GetTentacle(1));
 
-    ostringstream out;
-    PrintTree(out, "test_dir"_p);
-    assert(out.str() ==
-        "test_dir\n"
-        "  b\n"
-        "    f2.txt\n"
-        "    f1.txt\n"
-        "  a\n"
-        "    f3.txt\n"s
-    );
-    return 0;
+        // До присваивания octopus2 имеет своё собственное состояние
+        Octopus octopus2(10);
+
+        octopus2 = octopus1;
+
+        // После присваивания осьминогов щупальца копии имеют то же состояние,
+        // что и щупальца присваиваемого объекта
+        assert(octopus2.GetTentacleCount() == octopus1.GetTentacleCount());
+        for (int i = 0; i < octopus2.GetTentacleCount(); ++i) {
+            auto& tentacle1 = octopus1.GetTentacle(i);
+            auto& tentacle2 = octopus2.GetTentacle(i);
+            assert(&tentacle2 != &tentacle1);
+            assert(tentacle2.GetId() == tentacle1.GetId());
+            assert(tentacle2.GetLinkedTentacle() == tentacle1.GetLinkedTentacle());
+        }
+    }
+
+    // Проверка самоприсваивания осьминогов
+    {
+        Octopus octopus(3);
+
+        // Настраиваем состояние осьминога
+        octopus.GetTentacle(0).LinkTo(octopus.GetTentacle(1));
+
+        std::vector<std::pair<Tentacle*, Tentacle*>> tentacles;
+        // Сохраняем информацию о щупальцах осьминога и его копии
+        for (int i = 0; i < octopus.GetTentacleCount(); ++i) {
+            tentacles.push_back({ &octopus.GetTentacle(i), octopus.GetTentacle(i).GetLinkedTentacle() });
+        }
+
+        // Выполняем самоприсваивание
+        octopus = octopus;
+
+        // После самоприсваивания состояние осьминога не должно измениться
+        assert(octopus.GetTentacleCount() == static_cast<int>(tentacles.size()));
+        for (int i = 0; i < octopus.GetTentacleCount(); ++i) {
+            auto& tentacle_with_link = tentacles.at(i);
+            assert(&octopus.GetTentacle(i) == tentacle_with_link.first);
+            assert(octopus.GetTentacle(i).GetLinkedTentacle() == tentacle_with_link.second);
+        }
+    }
 }
