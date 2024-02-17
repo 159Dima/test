98fedddaa0afe39e98176b995825c1104ded9a64
159Dima <131018592+159Dima@users.noreply.github.com>
2024-02-17 13:55:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,207 +1,67 @@
-#include <cassert>
+#pragma once
+
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
-// Умный указатель, удаляющий связанный объект при своём разрушении.
-// Параметр шаблона T задаёт тип объекта, на который ссылается указатель
+#include <string_view>
+#include <deque>
+#include <map>
 using namespace std;
-template <typename T>
-class PtrVector {
+
+class Translator {
 public:
-    PtrVector() = default;
+    Translator() = default;
 
-    // Создаёт вектор указателей на копии объектов из other
-    PtrVector(const PtrVector& other) {
-        // Реализуйте копирующий конструктор самостоятельно
-        items_.reserve(other.GetItems().size());
-        for (auto object : other.GetItems()) {
-            
-            items_.push_back((object == nullptr) ? nullptr : (new T (*object)));
+    void Add(string_view source, string_view target) {
+        if (language1.count(source)) {
+            slova.push_back(string(target));
+            language1[source] = slova.back();
         }
-    }
-
-    // Деструктор удаляет объекты в куче, на которые ссылаются указатели,
-    // в векторе items_
-    ~PtrVector() {
-        // Реализуйте тело деструктора самостоятельно
-        for (auto& znach : items_) {
-            delete znach;
+        else{
+            slova.push_back(string(source));
+            slova.push_back(string(target));  
+            //cout << *(slova.end() - 2) << " " << slova.back() << endl;
+            language1[*(slova.end() - 2)] = slova.back();
+            language2[slova.back()] = *(slova.end() - 2);
         }
 
     }
-
-    // Возвращает ссылку на вектор указателей
-    std::vector<T*>& GetItems() noexcept {
-        // Реализуйте метод самостоятельно
-        return items_;
-    }
-
-    // Возвращает константную ссылку на вектор указателей
-    std::vector<T*> const& GetItems() const noexcept {
-        // Реализуйте метод самостоятельно
-        return items_;
-    }
-    PtrVector& operator=(const PtrVector& rhs) {
-        if (this != &rhs) {
-            // Реализация операции присваивания с помощью идиомы Copy-and-swap.
-            // Если исключение будет выброшено, то на текущий объект оно не повлияет.
-            auto rhs_copy(rhs);
-
-            // rhs_copy содержит копию правого аргумента.
-            // Обмениваемся с ним данными.
-            swap(rhs_copy);
-
-            // Теперь текущий объект содержит копию правого аргумента,
-            // а rhs_copy - прежнее состояние текущего объекта, которое при выходе
-            // из блока будет разрушено.
+    string_view TranslateForward(string_view source) const {
+        if (language1.count(source)) {
+            return language1.at(source);
         }
-
-        return *this;
+        return"";
     }
-private:
-    void swap(PtrVector& other) noexcept {
-        items_.swap(other.GetItems());
-    }
-
-
-
-    std::vector<T*> items_;
-};
-///////////////////////////////////////////////////////////////////////////////////////////////////
-class Tentacle {
-public:
-    explicit Tentacle(int id) noexcept
-        : id_(id) {
-    }
-
-    int GetId() const noexcept {
-        return id_;
-    }
-
-    Tentacle* GetLinkedTentacle() const noexcept {
-        return linked_tentacle_;
-    }
-    void LinkTo(Tentacle& tentacle) noexcept {
-        linked_tentacle_ = &tentacle;
-    }
-    void Unlink() noexcept {
-        linked_tentacle_ = nullptr;
+    string_view TranslateBackward(string_view target) const {
+        if (language2.count(target)) {
+            return language2.at(target);
+        }
+        return"";
     }
 
 private:
-    int id_ = 0;
-    Tentacle* linked_tentacle_ = nullptr;
+    deque<string> slova;
+    map<string_view, string_view> language1;
+    map<string_view, string_view> language2;
+
 };
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "my_assert.h"
+#include <string>
+#include <cassert>
 
-// Осьминог
-class Octopus {
-public:
-    Octopus()
-        : Octopus(8) {
-    }
+using namespace std;
+void TestSimple() {
+    Translator translator;
+    translator.Add(string("okno"s), string("window"s));
+    translator.Add(string("stol"s), string("table"s));
 
-    explicit Octopus(int num_tentacles) {
-        Tentacle* t = nullptr;
-        for (int i = 1; i <= num_tentacles; ++i) {
-            t = new Tentacle(i);
-            tentacles_.GetItems().push_back(t);
-            t = nullptr;
-        }
-    }
+    assert(translator.TranslateForward("okno"s) == "window"s);
+    assert(translator.TranslateBackward("table"s) == "stol"s);
+    assert(translator.TranslateForward("table"s) == ""s);
+    translator.Add(string("okno"s), string("winda"s));
+    assert(translator.TranslateForward("okno"s) != "window"s);
+}
 
-    ~Octopus() {
-        Cleanup();
-    }
-    Octopus& operator=(const Octopus& rhs) {
-        if (this != &rhs) {
-            tentacles_ = rhs.getV();
-        }
-
-        return *this;
-    }
-    Tentacle& AddTentacle() {
-        //Реализуйте добавление щупальца самостоятельно
-        Tentacle* t = new Tentacle(tentacles_.GetItems().size() + 1);
-        tentacles_.GetItems().push_back(t);
-        return *t;
-    }
-
-    int GetTentacleCount() const noexcept {
-        return static_cast<int>(tentacles_.GetItems().size());
-    }
-    PtrVector<Tentacle> getV() const {
-        return  tentacles_;
-    }
-    const Tentacle& GetTentacle(size_t index) const {
-        return *tentacles_.GetItems().at(index);
-    }
-    Tentacle& GetTentacle(size_t index) {
-        return *tentacles_.GetItems().at(index);
-    }
-
-private:
-    void Cleanup() {
-        // Удаляем щупальца осьминога из динамической памяти
-        for (Tentacle* t : tentacles_.GetItems()) {
-            delete t;
-        }
-        // Очищаем массив указателей на щупальца
-        tentacles_.GetItems().clear();
-    }
-
-
-    // Вектор хранит указатели на щупальца. Сами объекты щупалец находятся в куче
-    PtrVector<Tentacle> tentacles_;
-};
-// Эта функция main тестирует шаблон класса PtrVector
 int main() {
-    // Проверка присваивания осьминогов
-    {
-        Octopus octopus1(3);
-
-        // Настраиваем состояние исходного осьминога
-        octopus1.GetTentacle(2).LinkTo(octopus1.GetTentacle(1));
-
-        // До присваивания octopus2 имеет своё собственное состояние
-        Octopus octopus2(10);
-
-        octopus2 = octopus1;
-
-        // После присваивания осьминогов щупальца копии имеют то же состояние,
-        // что и щупальца присваиваемого объекта
-        assert(octopus2.GetTentacleCount() == octopus1.GetTentacleCount());
-        for (int i = 0; i < octopus2.GetTentacleCount(); ++i) {
-            auto& tentacle1 = octopus1.GetTentacle(i);
-            auto& tentacle2 = octopus2.GetTentacle(i);
-            assert(&tentacle2 != &tentacle1);
-            assert(tentacle2.GetId() == tentacle1.GetId());
-            assert(tentacle2.GetLinkedTentacle() == tentacle1.GetLinkedTentacle());
-        }
-    }
-
-    // Проверка самоприсваивания осьминогов
-    {
-        Octopus octopus(3);
-
-        // Настраиваем состояние осьминога
-        octopus.GetTentacle(0).LinkTo(octopus.GetTentacle(1));
-
-        std::vector<std::pair<Tentacle*, Tentacle*>> tentacles;
-        // Сохраняем информацию о щупальцах осьминога и его копии
-        for (int i = 0; i < octopus.GetTentacleCount(); ++i) {
-            tentacles.push_back({ &octopus.GetTentacle(i), octopus.GetTentacle(i).GetLinkedTentacle() });
-        }
-
-        // Выполняем самоприсваивание
-        octopus = octopus;
-
-        // После самоприсваивания состояние осьминога не должно измениться
-        assert(octopus.GetTentacleCount() == static_cast<int>(tentacles.size()));
-        for (int i = 0; i < octopus.GetTentacleCount(); ++i) {
-            auto& tentacle_with_link = tentacles.at(i);
-            assert(&octopus.GetTentacle(i) == tentacle_with_link.first);
-            assert(octopus.GetTentacle(i).GetLinkedTentacle() == tentacle_with_link.second);
-        }
-    }
+    TestSimple();
 }
