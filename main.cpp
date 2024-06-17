a9953ca4c6f1235e768f493d275075756fdb2c49
159Dima <131018592+159Dima@users.noreply.github.com>
2024-06-17 11:41:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,80 @@
-#pragma once
-
+#include <cassert>
+#include <string>
 #include <iostream>
-#include <string_view>
-#include <deque>
-#include <map>
+
 using namespace std;
 
-class Translator {
+class House {
+    // Реализуйте самостоятельно
 public:
-    Translator() = default;
-
-    void Add(string_view source, string_view target) {
-        if (language1.count(source)) {
-            slova.push_back(string(target));
-            language1[source] = slova.back();
-        }
-        else{
-            slova.push_back(string(source));
-            slova.push_back(string(target));  
-            //cout << *(slova.end() - 2) << " " << slova.back() << endl;
-            language1[*(slova.end() - 2)] = slova.back();
-            language2[slova.back()] = *(slova.end() - 2);
-        }
-
+    House(int length, int width, int hight): length_(length), width_(width), hight_(hight){}
+    int GetHeight() const {
+        return hight_;
     }
-    string_view TranslateForward(string_view source) const {
-        if (language1.count(source)) {
-            return language1.at(source);
-        }
-        return"";
+    int GetWidth() const{
+        return width_;
     }
-    string_view TranslateBackward(string_view target) const {
-        if (language2.count(target)) {
-            return language2.at(target);
-        }
-        return"";
+    int GetLength() const {
+        return length_;
     }
 
 private:
-    deque<string> slova;
-    map<string_view, string_view> language1;
-    map<string_view, string_view> language2;
+    int length_, width_, hight_;
+};
+
+class Resources {
+    // Реализуйте самостоятельно
+public:
+    Resources(int brick_count):brick_count_(brick_count){}
+    void TakeBricks(int count) {
+        if (count < 0 || count > brick_count_) throw std::out_of_range("недостаток керпичей");
+        brick_count_ -= count;
+    }
+    int GetBrickCount() const {
+        return brick_count_;
+    }
+private:
+    int brick_count_;
+};
+
+struct HouseSpecification {
+    int length = 0;
+    int width = 0;
+    int height = 0;
+};
+
+class Builder {
+public:
+    Builder(Resources& resoursces): resurc_(resoursces){}
+    House BuildHouse(HouseSpecification spec) {
+        int wail = (spec.length * spec.height * 4 * 8 + spec.width * spec.height * 4 * 8) * 2;
+        if (resurc_.GetBrickCount() < wail)throw std::runtime_error("мало керпичей");
+        resurc_.TakeBeicks(wail);
+        House haus(spec.length, spec.width, spec.height);
+        return haus;
+    }
+
+private:
+    Resources& resurc_;
 
 };
-///////////////////////////////////////////////////////////////////////////////////////////////
-#include <iostream>
-#include "my_assert.h"
-#include <string>
-#include <cassert>
-
-using namespace std;
-void TestSimple() {
-    Translator translator;
-    translator.Add(string("okno"s), string("window"s));
-    translator.Add(string("stol"s), string("table"s));
-
-    assert(translator.TranslateForward("okno"s) == "window"s);
-    assert(translator.TranslateBackward("table"s) == "stol"s);
-    assert(translator.TranslateForward("table"s) == ""s);
-    translator.Add(string("okno"s), string("winda"s));
-    assert(translator.TranslateForward("okno"s) != "window"s);
-}
 
 int main() {
-    TestSimple();
+    Resources resources{ 10000 };
+    Builder builder1{ resources };
+    Builder builder2{ resources };
+
+    House house1 = builder1.BuildHouse(HouseSpecification{ 12, 9, 3 });
+    assert(house1.GetLength() == 12);
+    assert(house1.GetWidth() == 9);
+    assert(house1.GetHeight() == 3);
+    cout << resources.GetBrickCount() << " bricks left"s << endl;
+
+    House house2 = builder2.BuildHouse(HouseSpecification{ 8, 6, 3 });
+    assert(house2.GetLength() == 8);
+    cout << resources.GetBrickCount() << " bricks left"s << endl;
+
+    House banya = builder1.BuildHouse(HouseSpecification{ 4, 3, 2 });
+    assert(banya.GetHeight() == 2);
+    cout << resources.GetBrickCount() << " bricks left"s << endl;
 }
